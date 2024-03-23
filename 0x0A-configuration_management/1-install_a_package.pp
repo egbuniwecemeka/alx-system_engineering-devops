291e0340809704c32dbf1b3231f8391ce5b764fe
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-03-22 20:28:14 -0700

    Execute a command

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,12 @@
-# A puppet script that installs flask using pip3
+# A puppet script that installs flask(2.1.0) using pip3
+# script also install Werkzeug(2.1.1)
 
-package {'flask':
-  ensure   => '2.1.0,
-  provider => 'pip3'
+exec {'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
 }
 
-package {'Werkzeug':
-  ensure   => '2.1.1',
-  provider => 'pip3'
+exec {'install_werkzeug':
+  command => '/usr/bin/pip3 install Werkzeug==2.1.1',
+  unless  => '/usr/bin/pip3 show Werkzeug | grep -q "Version: 2.1.1"',
 }
