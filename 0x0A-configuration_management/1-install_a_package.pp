55875c492de13a18aa322d233421576077e40e90
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-03-22 18:15:33 -0700

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,5 @@
 # Installing flask from pip3
 
-
 package {'python3-pip':
-  ensure => 'installed'
-}
-
-$flask_version = '2.1.0'
-
-exec {'install_flask':
-  command => "/usr/bin/pip3 install Flask==&{flask_version}",
-  require => Package['python3-pip']
+  ensure => 'installed',
 }
