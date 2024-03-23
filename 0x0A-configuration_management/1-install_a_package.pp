4eb4a9ae6e24bda0d4cd6acb0aa86ee5d2721d48
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-03-22 18:28:48 -0700

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -6,6 +6,6 @@ package {'python3-pip':
 
 exec {'install_flask':
   command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => ['/usr/bin/'],
+  path    => ['/usr/bin/pup'],
   require => Package['python3-pip']
 }
