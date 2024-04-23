fa96c7752e7f651a1a2ca54227a6ff98fdb03e82
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-04-22 18:35:25 -0700

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,7 +2,7 @@
 """
     Python script using REST API to return employee TODO list progress
     You must use urllib or requests module
-    must accept an integer as a parameter, which is the employee ID    
+    must accept an integer as a parameter, which is the employee ID
 """
 import requests
 import sys
@@ -12,24 +12,25 @@ if __name__ == "__main__":
 
     user_id = sys.argv[1]
 
-    #HTTP Req to API for user_id
+    # HTTP Req to API for user_id
     user_response = requests.get(url + "users/{}".format(user_id))
 
     # Convert response to dict
     user = user_response.json()
 
-    #HTTP Req to API to retrieve todo_list using user_id
+    # HTTP Req to API to retrieve todo_list using user_id
     user_attr = {"userId": user_id}
     todo_response = requests.get(url + "todos", user_attr)
 
-    #Convert response to dict/JSON
+    # Convert response to dict/JSON
     todos = todo_response.json()
 
-    #List to store completed task
+    # List to store completed task
     complete = [t.get("title") for t in todos if t.get("completed") is True]
 
     print("Employee {} is done with tasks({}/{}):".format(user.get("name"),
-                                                         len(complete), len(todos)))
-    
+                                                          len(complete),
+                                                          len(todos)))
+ 
     for c in complete:
         print("\t {}".format(c))
