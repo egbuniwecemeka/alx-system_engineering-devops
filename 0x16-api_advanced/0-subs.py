b7504f3a2974fcf2ec86c4e9b8ec926d02ea1a6d
egbuniwecemeka <egbuniwecemeka.com>
2024-08-13 22:06:56 +0100

    How many subs?

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" A python script that queries Reddit API and returns the number of subscribers """
+
+import requests
+
+def number_of_subscribers(subreddit):
+    """ Queries Reddit API and returns total number of subscribers"""
+
+    url = "https://www.reddit.com/r/{}/about-json".format(subreddit)
+
+    headers = {"User-Agent": "Mozilla/5.0"}
+    request = requests.get(url, headers=headers, allow_redirects=False)
+    data = request.json()
+    if data == 200:
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
