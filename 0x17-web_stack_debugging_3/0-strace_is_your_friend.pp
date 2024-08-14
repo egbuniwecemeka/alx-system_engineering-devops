69ab8ca11d8b0ba96c31cf5ffbcd52ea43a38c00
egbuniwecemeka <egbuniwecemeka@gmail.com>
2024-08-14 21:37:46 +0000

    Strace is your friend

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# A puppet script that fixes a faulty "phpp" extension to "php"
+
+exec{'wordpress-fix':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+} 
