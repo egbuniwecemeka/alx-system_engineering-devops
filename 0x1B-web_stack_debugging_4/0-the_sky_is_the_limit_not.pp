db1f33b687a8edce0f5ee4b5c8738d48306ac523
egbuniwecemeka <egbuniwecemeka.com>
2024-08-22 20:30:27 +0100

    restart nginx service

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,6 @@
 # A puppet file that resets the ulimit of a request
 
+# Modify ULIMT of server default's file
 exec{'fix-ulimit':
   # Updating the ulimit value
   command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
@@ -7,3 +8,5 @@ exec{'fix-ulimit':
   # Path to config file containing ulimit value
   path    => '/usr/local/bin/:/bin'
 }
+
+
