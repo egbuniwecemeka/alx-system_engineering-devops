900452dc7d35ffa5c1d3a80e3d2f1d9b7938c8e7
egbuniwecemeka <egbuniwecemeka.com>
2024-08-22 20:43:21 +0100

    restart nginx service

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -5,8 +5,15 @@ exec{'fix-ulimit':
   # Updating the ulimit value
   command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
 
-  # Path to config file containing ulimit value
+  # Path to sed executable for sed command
   path    => '/usr/local/bin/:/bin'
 }
 
+# Restart nginx
+exec{'restart-nginx':
+  # Restart nginx server
+  command => '/etc/init.d/nginx restart',
 
+  # Path to config file.
+  path    => '/etc/init.d/'
+}
